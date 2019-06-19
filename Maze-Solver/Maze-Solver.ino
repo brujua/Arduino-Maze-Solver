d7db547868928295b550e9ae7a585e0c6a232710
brujua <brunocrisafulli@yahoo.com.ar>
2019-06-18 22:30:41 -0300

    implement re-center correction
    allow the robot to follow an straight line continously.

--- Maze-Solver/Maze-Solver.ino
+++ Maze-Solver/Maze-Solver.ino
@@ -1,10 +1,12 @@
 
 int frwd_counter = 0;
-
+const int MOVE_TIME = 250;
+const int CENTER_SEARCH_MOVE_LAPSE = 50;
+const int SEARCH_ITERS = 15;
 void setup() {
-  pinMode(D0  , INPUT) ; //Sensor infrarrojo como entrada
-  pinMode( D1 , INPUT) ;
-  pinMode( D2, INPUT) ;
+  pinMode(D0, INPUT); //Sensor infrarrojo como entrada
+  pinMode(D1, INPUT);
+  pinMode(D2, INPUT);
   pinMode(D5, OUTPUT);
   pinMode(D6, OUTPUT);
   pinMode(D7, OUTPUT);
@@ -14,32 +16,19 @@ void setup() {
 }
 
 void loop() {
-  delay(1000);
-  frwd_counter++; 
-  int valor1 = digitalRead(D0) ; //leemos el valor del sensor infrarrojo
-  int valor2 = digitalRead(D1) ;
-  int valor3 = digitalRead(D2) ;
-  Serial.println("counter:");
-  Serial.println(frwd_counter);    
-  print_sensors(valor1, valor2, valor3);
-  if(frwd_counter >= 10){
-    frwd_counter = 0;
+  //delay(100);
+  //frwd_counter++; 
+  int right_value = digitalRead(D0); //leemos el valor del sensor infrarrojo
+  int center_value = digitalRead(D1);
+  int left_value = digitalRead(D2);    
+  print_sensors(right_value, center_value, left_value);
+  if (center_value) {
     go_forward();
+  } else {
+    bool center_found = find_center();
+    if(!center_found)
+      delay(10000); 
   }
-
-  
-  /*MotorHorario();
-  Serial.println("Giro del Motor en sentido horario");
-  delay(1000);
-  
-  MotorAntihorario();
-  Serial.println("Giro del Motor en sentido antihorario");
-  delay(5000);
-  
-  MotorStop();
-  Serial.println("Motor Detenido");
-  delay(5000);*/
-  
 }
 
 void print_sensors(int sensor_right, int sensor_center, int sensor_left){
@@ -61,8 +50,35 @@ void go_forward(){
   digitalWrite (D7, LOW);
   digitalWrite (D8, HIGH);
 
-  delay(700);
-  motors_stop();
+  delay(MOVE_TIME);
+  stop_motors();
+}
+
+bool find_center(){
+  int center = digitalRead(D1);
+  Serial.println("BUSCANDO HACIA LA DERECHA");
+  //arc to the right in search for the center strip
+  for(int i=0; !center && i<SEARCH_ITERS; i++){
+    move_right(CENTER_SEARCH_MOVE_LAPSE);
+    delay(100);
+    center = digitalRead(D1);
+  }
+  if (center)
+    return true;
+  delay(3000);
+  
+  Serial.println("BUSCANDO HACIA LA IZQUIERDA");
+  //arc to the left in search for the center strip, have to account for the previous move to the right
+  int search_iters = SEARCH_ITERS * 3;
+  for(int i=0; !center && i<search_iters; i++){
+    move_left(CENTER_SEARCH_MOVE_LAPSE);
+    delay(100);
+    center = digitalRead(D1); 
+  }
+  if(center)
+    return true;
+  else
+    return false;  
 }
 
 void go_backwards(){
@@ -74,11 +90,27 @@ void go_backwards(){
   digitalWrite (D8, HIGH);
 
   delay(1000);
-  motors_stop();
+  stop_motors();
+}
+
+void move_left(int time_ms){
+  // move Right wheel motor to move left
+  digitalWrite (D5, LOW);
+  digitalWrite (D6, HIGH);
+  delay(time_ms);
+  stop_motors();
+}
+
+void move_right(int time_ms){
+  // move left wheel motor to move right
+  digitalWrite (D7, LOW);
+  digitalWrite (D8, HIGH);
+  delay(time_ms);
+  stop_motors();
 }
 
 
-void motors_stop()
+void stop_motors()
 {
   digitalWrite (D5, LOW);
   digitalWrite (D6, LOW);
